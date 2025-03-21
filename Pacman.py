199d3a161ba3163f280d763bb0382842a123ad3b
phant0m15 <murkalomakz24@gmail.com>
2025-03-21 14:13:02 +0200

    Коміт 2: Визначення шляху до кореня проекту та директорій

--- Pacman.py
+++ Pacman.py
@@ -4,3 +4,14 @@ import pygame_gui  # необхідні бібліотеки
 import os
 
 pygame.init()
+
+# Путь к корню вашего проекта
+project_root = r'C:\Users\mnebr\PycharmProjects\PythonProject'
+
+# Директории, с которыми вы работаете
+directories = ['characters', 'maps', 'screenshots', 'sounds']
+
+# Получаем абсолютные пути для каждой из директорий
+abs_paths = {dir_name: os.path.join(project_root, dir_name) for dir_name in directories}
+
+# Теперь переменная abs_paths доступна для использования во всех функциях и частях кода
